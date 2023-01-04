6eeafcd0127a2054de5ed41a5c2b29e158057b6e
takahiro-okada <okahanako09875@gmail.com>
2023-01-04 23:03:03 +0900

    feat:Add maker table

--- sql/database.sql
+++ sql/database.sql
@@ -11,3 +11,15 @@ DROP TABLE IF EXISTS clock;
   INSERT INTO clock (clock_name, clock_description) VALUES ("スモールワールド","説明が入ります｡");
   INSERT INTO clock (clock_name, clock_description) VALUES ("パルタージュ","説明が入ります｡");
   INSERT INTO clock (clock_name, clock_description) VALUES ("シンフォニー","説明が入ります｡");
+
+DROP TABLE IF EXISTS maker;
+
+   CREATE TABLE maker (
+    id int unsigned AUTO_INCREMENT,
+    maker_name VARCHAR(100) NOT NULL,
+    clock_id int,
+    PRIMARY KEY(id)
+   );
+
+    INSERT INTO maker ( maker_name, clock_id) VALUES ("SEIKO", 1)
+    INSERT INTO maker ( maker_name, clock_id) VALUES ("CITIZEN", 2)
