972d81c197f8660b1fdf937d408a653a28a07f98
takahiro-okada <okahanako09875@gmail.com>
2023-01-09 15:57:18 +0900

    feat:Add cross origins settings

--- src/main/java/com/example/demo/controller/MakerController.java
+++ src/main/java/com/example/demo/controller/MakerController.java
@@ -3,10 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.entity.Maker;
 import com.example.demo.service.MakerService;
 import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class MakerController {
 
   private final MakerService makerService;
